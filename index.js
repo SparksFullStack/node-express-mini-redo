5f94f90d895c1779360d8295f706e67cddb0f0f7
SparksFullStack <csparks@antioch.edu>
2018-07-25 13:12:46 -0700

    get to /api/users/:id finished and working

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ server.listen(port, () => console.log(`The server is listening on port ${port}`)
 
 // GET to /api/users
 server.get('/api/users', (req, res) => {
-    const users = find()
+    find()
         .then(response => res.json(response))
         .catch(err => {
             res
@@ -19,7 +19,27 @@ server.get('/api/users', (req, res) => {
 })
 
 // GET to /api/users/:id
-
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    if (!id) res.status(404).json({ error: 'There was no ID provided with your request' });
+    findById(id)
+        .then(response => {
+            if (!response[0]){
+                res
+                    .status(404)
+                    .json({ message: 'The user with the specified ID does not exist.' });
+            } else {
+                res
+                    .status(200)
+                    .json(response[0]);
+            } 
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ error: 'The user information could not be retrieved.' });
+        })
+})
 // POST to /api/users
 
 // DELETE to /api/users/:id
